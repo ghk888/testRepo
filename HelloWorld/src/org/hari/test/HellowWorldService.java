d319748783770ff1fa373d43085bcbf570ec6225
Hari Golla <gkrishna.java@gmail.com>
2017-01-08 01:31:59 -0500

    Changes to "New Feautre branch"

--- HelloWorld/src/org/hari/test/HellowWorldService.java
+++ HelloWorld/src/org/hari/test/HellowWorldService.java
@@ -3,7 +3,7 @@ package org.hari.test;
 public class HellowWorldService {
 	
 	public static void main(String[] args) {
-		System.out.println("Im Hellow World Service");
+		System.out.println("Im Hellow World Service2");
 		
 		
 		System.out.println("Im Hellow World Service..Changes ");
