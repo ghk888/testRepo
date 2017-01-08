3d70b73a4f2a2812b323fa7012dcf48528e1d837
Hari Golla <gkrishna.java@gmail.com>
2017-01-08 00:29:04 -0500

    My 4th Message

--- HelloWorld/src/org/hari/test/HelloService3.java
+++ HelloWorld/src/org/hari/test/HelloService3.java
@@ -4,7 +4,12 @@ public class HelloService3 {
 
 	
 	public static void main(String[] args) {
-		System.out.println("Hi Im HelloService 3");
+		System.out.println("Hi Im HelloService 3...");
+		System.out.println("Hi Im HelloService 4...");
+		
+		
+		
+		System.out.println("Hi Im HelloService 5...");
 	}
 	
 }
--- HelloWorld/src/org/hari/test/HellowWorldService.java
+++ HelloWorld/src/org/hari/test/HellowWorldService.java
@@ -4,6 +4,9 @@ public class HellowWorldService {
 	
 	public static void main(String[] args) {
 		System.out.println("Im Hellow World Service");
+		
+		
+		System.out.println("Im Hellow World Service..Changes ");
 	}
 
 }
