8374749f7c304d3f0b7bca1d371fe79b714a2915
Hari Golla <gkrishna.java@gmail.com>
2017-01-08 00:17:57 -0500

    My 3rd Commit.

---
+++ HelloWorld/src/org/hari/test/HelloService3.java
@@ -0,0 +1,10 @@
+package org.hari.test;
+
+public class HelloService3 {
+
+	
+	public static void main(String[] args) {
+		System.out.println("Hi Im HelloService 3");
+	}
+	
+}
---
+++ HelloWorld/src/org/hari/test/HellowWorldService.java
@@ -0,0 +1,9 @@
+package org.hari.test;
+
+public class HellowWorldService {
+	
+	public static void main(String[] args) {
+		System.out.println("Im Hellow World Service");
+	}
+
+}
